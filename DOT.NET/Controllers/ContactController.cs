c839c15b04c1e31a63872b9996c76a40d27287e9
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-09-04 20:29:21 +0400

    Testimonial_Contact_CTRL

---
+++ DOT.NET/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOT.NET.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILocationManager _locationManager;
+
+        public ContactController(ILocationManager locationManager)
+        {
+            _locationManager = locationManager;
+        }
+
+        public IActionResult Index()
+        {
+            var location = _locationManager.GetFirst();
+            return View(location);
+        }
+    }
+}
---
+++ DOT.NET/Controllers/TestimonialController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOT.NET.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IClientManager _clientManager;
+
+        public TestimonialController(IClientManager clientManager)
+        {
+            _clientManager = clientManager;
+        }
+
+        public IActionResult Index()
+        {
+            var clients = _clientManager.GetAll();
+            return View(clients);
+        }
+    }
+}
